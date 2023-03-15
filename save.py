ff1e17bd46570d1702c5c9bc898b4d2f7b300f98
Hexona <xuanjiap@lolicon.ac.cn>
2023-03-15 23:54:33 +0900

    update

--- save.py
+++ save.py
@@ -3,7 +3,7 @@ from transformers import AutoTokenizer, AutoModel
 tokenizer = AutoTokenizer.from_pretrained(
     "THUDM/chatglm-6b", trust_remote_code=True)
 model = AutoModel.from_pretrained(
-    "THUDM/chatglm-6b", trust_remote_code=True).half().quantize(8).cuda()
+    "THUDM/chatglm-6b", trust_remote_code=True)
 model.eval()
 
 _ = model.save_pretrained("./model")
