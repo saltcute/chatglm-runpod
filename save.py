f8dfd3943b9e0a826d5aa9c098b7398bab0670ef
Hexona <xuanjiap@lolicon.ac.cn>
2023-03-15 23:54:51 +0900

    update

--- save.py
+++ save.py
@@ -4,7 +4,6 @@ tokenizer = AutoTokenizer.from_pretrained(
     "THUDM/chatglm-6b", trust_remote_code=True)
 model = AutoModel.from_pretrained(
     "THUDM/chatglm-6b", trust_remote_code=True)
-model.eval()
 
 _ = model.save_pretrained("./model")
 _ = tokenizer.save_pretrained("./model")
